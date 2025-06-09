955507edca6eec5304fc85de18cf078b2d94bc47
Antonius Atef Ghaly <aaghaly.vpn>
2025-06-09 20:59:40 +0300

    fix-author-seeding-data

--- LMS.DAL/Data/Models/Author.cs
+++ LMS.DAL/Data/Models/Author.cs
@@ -1,26 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LMS.DAL.Data.Models
+﻿namespace LMS.DAL.Data.Models
 {
-    public class Author:ISharedColumns
+    public class Author : ISharedColumns
     {
         public int Id { get; set; }
         public string FullName { get; set; } = null!;
         public string? Description { get; set; }
         public DateOnly DateOfBirth { get; set; }
-        public string? ImageUrl { get; set; }   
+        public string? ImageUrl { get; set; }
         public ICollection<Book> Books { get; set; } = new HashSet<Book>();
-        public DateTime?InsertedTime { get; set; }
+        public DateTime? InsertedTime { get; set; }
         public string? InsertedUserId { get; set; }
-        public bool IsActive { get; set; }
+        public bool IsActive { get; set; } = true;
         public DateTime? ActivationTime { get; set; }
         public string? ActivationUserId { get; set; }
-        public DateTime?UpdateTime { get; set; }
-        public string?UpdateUserId { get; set; }
+        public DateTime? UpdateTime { get; set; }
+        public string? UpdateUserId { get; set; }
         public bool IsDeleted { get; set; }
         public DateTime? DeletedTime { get; set; }
         public string? DeletedUserId { get; set; }
